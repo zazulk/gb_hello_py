fdf4bf7f85c62ba90ea7726f8f1d717953fed9b6
Zulk <svsmir32@mts.ru>
2022-06-02 00:27:43 +0300

    [fix] edit logic

--- lesson 2 300522/task 4.py
+++ lesson 2 300522/task 4.py
@@ -2,13 +2,25 @@
 # Вывести каждое слово с новой строки. Строки нужно пронумеровать. Если слово
 # длинное, выводить только первые 10 букв в слове.
 
-phrase = input("Скажите какую-нибудь фразу: ")
+# phrase = input("Скажите какую-нибудь фразу: ").strip().replace("  ", "")
+# words = phrase.split()
 
-phrase = phrase.replace("  ", "")
+print("Буду выводить нумерованный список слов.")
+words = []
+phrase = ""
 punctuations = [",", ".", "!", ":", "–", "?", ";"]
-for mark in punctuations:
-    phrase = phrase.replace(mark, "")
 
+while len(words) <= 1:
+    if len(words) == 0:
+        phrase = input("Скажите какую-нибудь фразу: ")
+    if len(words) == 1:
+        print("⚠️ Этого мало.")
+        phrase += " " + input("Еще что-нибудь скажите: ")
+    for mark in punctuations:
+        phrase = phrase.replace(mark, " ")
+    words = phrase.split()
+
+print("\nРезультат:")
 words = phrase.split()
 for ind, word in enumerate(words):
     print(ind, word[:11])
