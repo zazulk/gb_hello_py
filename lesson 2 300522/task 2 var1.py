c279951656d2034619d6a7c960c5f08ba087fe5a
Zulk <svsmir32@mts.ru>
2022-06-02 23:21:30 +0300

    [fix] добавили стоппер

--- lesson 2 300522/task 2 var1.py
+++ lesson 2 300522/task 2 var1.py
@@ -5,21 +5,27 @@
 
 
 inp = input("Я переставлю слова местами. Скажите что-нибудь: ").strip()
+stop_words = ["q", "й", "стоп", "stop", "cnjg", "флюгегехаймен"]
+stop = False
 
 while len(inp) <= 1:
+    if inp in stop_words:
+        print("Ну и ладно :(")
+        stop = True
     if len(inp) == 0:
-        inp = input("Скажите что-нибудь: ")
+        inp = input("Скажите что-нибудь или введите 'Q' для выхода: ")
     if len(inp) == 1:
         print("⚠️ Этого мало.")
         inp += " " + input("Еще что-нибудь скажите: ")
 
-words = inp.strip().split()
+if not stop:
+    words = inp.strip().split()
 
-for ind in range(len(words)):
-    if (ind + 1) % 2 == 0:
-        actual = words[ind]
-        last = words[ind-1]
-        words[ind-1], words[ind] = actual, last
+    for ind in range(len(words)):
+        if (ind + 1) % 2 == 0:
+            actual = words[ind]
+            last = words[ind-1]
+            words[ind-1], words[ind] = actual, last
 
-print("Результат перестановки слов:")
-print(" ".join(words))
+    print("Результат перестановки слов:")
+    print(" ".join(words))
--- lesson 2 300522/task 2 var2.py
+++ lesson 2 300522/task 2 var2.py
@@ -4,20 +4,26 @@
 # списка элементов нужно использовать функцию input().
 
 inp = input("Я переставлю слова местами. Скажите что-нибудь: ").strip()
+stop_words = ["q", "й", "стоп", "stop", "cnjg", "флюгегехаймен"]
+stop = False
 
 while len(inp) <= 1:
+    if inp in stop_words:
+        print("Ну и ладно :(")
+        stop = True
     if len(inp) == 0:
-        inp = input("Скажите что-нибудь: ")
+        inp = input("Скажите что-нибудь или введите 'Q' для выхода: ")
     if len(inp) == 1:
         print("⚠️ Этого мало.")
         inp += " " + input("Еще что-нибудь скажите: ")
 
-words = inp.strip().split()
+if not stop:
+    words = inp.strip().split()
 
-for k in range(1, len(words), 2):
-    actual = words[k]
-    last = words[k - 1]
-    words[k - 1], words[k] = actual, last
+    for k in range(1, len(words), 2):
+        actual = words[k]
+        last = words[k - 1]
+        words[k - 1], words[k] = actual, last
 
-print("Результат перестановки слов:")
-print(" ".join(words))
+    print("Результат перестановки слов:")
+    print(" ".join(words))
