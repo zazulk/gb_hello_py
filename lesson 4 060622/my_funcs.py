f24cd33308cba4802c194b3102342e579039da02
Zulk <svsmir32@mts.ru>
2022-06-09 10:09:52 +0300

    [feat] get_numb_from_user

--- lesson 4 060622/my_funcs.py
+++ lesson 4 060622/my_funcs.py
@@ -1,3 +1,6 @@
+import random
+
+
 def is_stop(text):
     """Проверяет, является ли текст стоп-словом
 
@@ -27,3 +30,73 @@ def is_numb_str(text):
         return True
     except Exception:
         return False
+
+
+def get_numb_from_user(question, try_limit=None, rand_n_min=None,
+                       rand_n_max=None, kind_of_numb="int", positive=False,
+                       negative=False, is_with_zero=True):
+    res_n = None
+    if not question:
+        return None
+    numb_inp = ""
+    pos_or_neg_adj_pl = ""
+
+    if positive and not negative:
+        pos_or_neg_adj_pl = "положительные"
+    elif negative and not positive:
+        pos_or_neg_adj_pl = "отрицательные"
+
+    if not rand_n_min and rand_n_min != 0 or rand_n_min >= rand_n_max:
+        if negative and not positive:
+            rand_n_min = -5.0 if kind_of_numb == "float" else -5
+        else:
+            rand_n_min = 5.0 if kind_of_numb == "float" else 5
+    if not rand_n_max and rand_n_max != 0 or rand_n_min >= rand_n_max:
+        if negative and not positive:
+            rand_n_max = -15.0 if kind_of_numb == "float" else -15
+        else:
+            rand_n_max = 15.0 if kind_of_numb == "float" else 15
+
+    kind_adj_pl = "дробные" if kind_of_numb == "float" else "целые"
+    try_count = 0
+
+    while not numb_inp:
+        if try_limit and try_count >= try_limit:
+            if kind_of_numb == "float":
+                if all([type(el) is float for el in [rand_n_min, rand_n_max]]):
+                    res_n = random.uniform(rand_n_min, rand_n_max)
+                else:
+                    res_n = random.uniform(5.0, 15.0)
+            else:
+                if all([type(el) is int for el in [rand_n_min, rand_n_max]]):
+                    res_n = random.randint(rand_n_min, rand_n_max)
+                else:
+                    res_n = random.randint(5, 15)
+            print(f"{'':>5}Так, ладно, сам выберу. Выбрал {res_n}.")
+            break
+
+        numb_inp = input(f"{question} ").replace(",", ".").strip()
+        if numb_inp == "":
+            continue
+        if is_stop(numb_inp):
+            print("Ну и ладно :(")
+            break
+        try:
+            if kind_of_numb == "int":
+                res_n = int(numb_inp)
+            if kind_of_numb == "float":
+                res_n = float(numb_inp)
+            is_error = False
+        except Exception as err:
+            print(f"❌ err = {err}")
+            is_error = True
+        if is_error or (positive and not negative and res_n < 0) or \
+                (not is_with_zero and res_n == 0):
+            print(
+                f"{'⚠️':>8} Нужно вводить {kind_adj_pl}"
+                f"{(' ' + pos_or_neg_adj_pl) if pos_or_neg_adj_pl else ''}"
+                f" числа.")
+            numb_inp = ""
+            res_n = None
+            continue
+    return res_n
--- lesson 4 060622/task 6 var1.py
+++ lesson 4 060622/task 6 var1.py
@@ -7,62 +7,23 @@
 from itertools import count
 from itertools import cycle
 from itertools import islice
-from my_funcs import is_stop
-from my_funcs import is_float_numb
-from my_funcs import is_numb_str
+from my_funcs import get_numb_from_user
 import random
 
 print("Вводите данные или 'Q' для выхода.")
 
-start_n = 0
-max_count = 0
-try_count = 0
-result = []
-stop = False
+max_count = None
+quest_1 = "целое число, начиная с которого будем генерировать:"
+start_n = get_numb_from_user(quest_1, kind_of_numb="int", positive=True,
+                             negative=True)
 
-while not start_n:
-    numb_inp = input(f"{'':>4} целое число, начиная с которого будем "
-                     f"генерировать: ").strip()
-    if numb_inp == "":
-        continue
-    if is_stop(numb_inp):
-        stop = True
-        break
-    if is_float_numb(numb_inp) or not is_numb_str(numb_inp):
-        print(f"{'⚠️':>8} Нужно вводить целые числа.")
-        continue
-    start_n = int(numb_inp)
-    if start_n == 0:
-        break
+if start_n is not None:
+    max_count = get_numb_from_user("сколько чисел сгенерировать:", try_limit=3,
+                                   kind_of_numb="int", positive=True,
+                                   negative=False, is_with_zero=False)
 
-if stop:
-    print("Ну и ладно :(")
-
-if not stop:
-    while not max_count:
-        if try_count >= 3:
-            max_count = random.randint(5, 15)
-            print(f"{'':>5}Так, ладно, сам выберу. Сгенерирую {max_count} чисел.")
-            break
-        max_inp = input(f"{'':>5}сколько чисел сгенерировать: ").strip()
-        if not max_inp:
-            try_count += 1
-            continue
-        if is_stop(max_inp):
-            stop = True
-            break
-        if is_float_numb(max_inp) or not is_numb_str(max_inp):
-            try_count += 1
-            print(f"{'⚠️':>8} Нужно вводить целые числа.")
-            continue
-        max_count = int(max_inp)
-        if max_count <= 0:
-            max_count = 0
-            try_count += 1
-            print(f"{'⚠️':>8} Количество не может быть нулем или "
-                  f"отрицательным.")
-            continue
-
+if start_n is not None and max_count is not None:
+    result = []
     for item in count(start_n):
         result.append(item)
         print(f"{'':>5}{item}")
