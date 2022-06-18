19f4bbddc1fb2a76ff690f7194288a82f96d9052
Zulk <svsmir32@mts.ru>
2022-06-19 02:01:11 +0300

    [fix] поправили логику для police_car + косметика

--- lesson 6 130622/task 4.py
+++ lesson 6 130622/task 4.py
@@ -4,70 +4,72 @@ from random import choice
 
 class Car:
     speed = 40
-    color = "red"
+    color = "\033[30m"
     name = "Car"
     is_police = False
 
-    def __init__(self, speed, color, name, is_police):
+    def __init__(self, speed, color, name, is_police=False):
         self.speed, self.color, self.name, self.is_police = speed, color, name,\
                                                             is_police
 
     def go(self):
-        print("Поехали!")
+        print("\tПогнали!")
 
     def stop(self):
-        print("Тормози.")
+        print("\tТормози.")
 
     def turn(self, direction):
-        print(f"Двигай {direction}")
+        print(f"\tДвигай {direction}!")
 
     def show_speed(self):
-        print(f"Скорость: {self.speed}")
+        print(f"\tСкорость: {self.speed}км/ч.")
 
 
 class TownCar(Car):
     pic = "🚙"
 
     def go(self):
-        print("Газку!")
+        print("\tГазку!")
 
     def show_speed(self):
+        super().show_speed()
         if self.speed > 60:
-            print(f"🚨 Нарушаем, нарушаем! Превышение {self.speed - 60}км")
-        else:
-            print(f"Скорость: {self.speed}")
+            print(f"\t🚨 Нарушаем, нарушаем! Превышение на \033[31m{self.speed - 60}\033[0mкм")
 
 
 class SportCar(Car):
     pic = "🏎"
 
     def go(self):
-        print("Тапку в пол!")
+        print("\tТапку в пол!")
 
 
 class WorkCar(Car):
     pic = "🚛"
 
     def go(self):
-        print("Газуй!")
+        print("\tГазуй!")
 
     def show_speed(self):
+        super().show_speed()
         if self.speed > 40:
-            print(f"🚨 Нарушаем, нарушаем! Превышение {self.speed - 40}км")
-        else:
-            print(f"Скорость: {self.speed}")
+            print(f"\t🚨 Нарушаем, нарушаем! Превышение на \033[31m{self.speed - 40}\033[0mкм")
 
 
 class PoliceCar(Car):
     pic = "🚓"
 
+    def __init__(self, speed, color, name):
+        self.color = "\033[47m\033[34m"
+        super().__init__(speed, self.color, name, is_police=True)
 
-def car_generator(car_type):
+
+def car_factory(car_type):
     colors = [
-        "\033[31m",
-        "\033[32m",
-        "\033[33m",
-        "\033[34m",
+        "\033[41m\033[30m",
+        "\033[42m\033[30m",
+        "\033[43m\033[30m",
+        "\033[45m\033[30m",
     ]
     car_types = {
         "sport": {
@@ -95,17 +97,14 @@ def car_generator(car_type):
     }
     res_type = car_types.get(car_type)
     res_speed = choice(res_type.get("speeds"))
-    is_police = car_type == "police"
     example = res_type.get("class")(speed=res_speed, color=choice(colors),
-                                    name=choice(res_type.get("names")),
-                                    is_police=is_police)
+                                    name=choice(res_type.get("names")))
     return example
 
 
 for el in ["sport", "town", "police", "work"]:
-    car = car_generator(el)
-    print(f"{car.pic} {car.color}{car.name}\033[0m")
-    print(f"is police: {car.is_police}")
+    car = car_factory(el)
+    print(f"{car.pic} {car.color}\033[1m {car.name} \033[0m")
     car.go()
     car.show_speed()
     car.stop()
