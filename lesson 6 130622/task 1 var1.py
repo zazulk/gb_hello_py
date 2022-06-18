072ddef45295f8195fdfcba693dab536e6d0f606
Zulk <svsmir32@mts.ru>
2022-06-19 02:00:36 +0300

    [fix] новый светофор

--- lesson 6 130622/task 1 var1.py
+++ lesson 6 130622/task 1 var1.py
@@ -1,20 +1,25 @@
 import time
 from random import randint
+import sys
+
+# Вариант 1. Перемещаемся туда сюда по строкам терминал и печатаем кружочки
+# ⚠️ Нужно включить настройку "Emulate terminal in output console"
 
 
 class TrafficLight:
+    __color = "yellow"
     __colors = {
         "red": {
             "pause": 7,
-            "txt_color": "\033[31m🔴"
+            "emoji": "🔴"
         },
         "yellow": {
             "pause": 2,
-            "txt_color": "\033[33m🟡"
+            "emoji": "🟡"
         },
         "green": {
             "pause": randint(4, 7),
-            "txt_color": "\033[32m🟢"
+            "emoji": "🟢"
         },
     }
 
@@ -23,13 +28,28 @@ class TrafficLight:
             return
         start = time.monotonic()
         result = 0
+        colors = list(self.__colors.keys())
+        colors.append(self.__color)
+        # count = 0
         while result <= limit_secs:
-            for key, value in self.__colors.items():
-                print(f"{value.get('txt_color')} {key}")
-                time.sleep(value.get("pause"))
+            for color in colors:
+                # Если не хотим, чтобы криво отображался первый кружочек 😬
+                # count += 1
+                # if color == "red" and count > 1:
+                if color == "red":
+                    sys.stdout.write('\r\x1b[2A')
+
+                color_info = self.__colors.get(color)
+                print(f"{color_info.get('emoji')}", end='')
+                time.sleep(color_info.get("pause"))
+                sys.stdout.write('\r\x1b[2K')
+                print(f"\r⚫️")
+                if color == "green":
+                    sys.stdout.write('\x1b[2F')
                 result = time.monotonic() - start
+
+        sys.stdout.write('\x1b[2B')
 
 
 traffic_lighter = TrafficLight()
 traffic_lighter.running(randint(20, 50))
-
--- lesson 6 130622/task 1 var2.py
+++ lesson 6 130622/task 1 var2.py
@@ -1,5 +1,9 @@
 import time
 from random import randint
+import sys
+
+# Вариант 2. Все кружочки одна строка
+# ⚠️ Нужно включить настройку "Emulate terminal in output console"
 
 
 class TrafficLight:
@@ -7,15 +11,15 @@ class TrafficLight:
     __colors = {
         "red": {
             "pause": 7,
-            "txt_color": "\033[31m🔴"
+            "lighter": "🔴\n⚫️\n⚫️"
         },
         "yellow": {
             "pause": 2,
-            "txt_color": "\033[33m🟡"
+            "lighter": "⚫️\n🟡\n⚫️"
         },
         "green": {
             "pause": randint(4, 7),
-            "txt_color": "\033[32m🟢"
+            "lighter": "⚫️\n⚫️\n🟢"
         },
     }
 
@@ -27,13 +31,14 @@ class TrafficLight:
         colors = [self.__color]
         colors.extend(list(self.__colors.keys()))
 
-
         while result <= limit_secs:
             for color in colors:
                 color_info = self.__colors.get(color)
-                print(f"{color_info.get('txt_color')} {color}")
+                print(f"\r{color_info.get('lighter')}", end="")
                 time.sleep(color_info.get("pause"))
+                sys.stdout.write('\r\x1b[2A')
                 result = time.monotonic() - start
+        sys.stdout.write('\x1b[2B')
 
 
 traffic_lighter = TrafficLight()
